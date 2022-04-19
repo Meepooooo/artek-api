1a4a88752cfbc5ce06906b4cc8f50a6d2e4af630
baka <dyatlovleonid@gmail.com>
2022-04-19 11:34:54 +0300

    a

--- db.go
+++ db.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/TaeKwonZeus/artek-api/config"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 var schema = `
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/TaeKwonZeus/artek-api/config"
-	_ "github.com/lib/pq"
 )
 
 func main() {
