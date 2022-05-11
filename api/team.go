92e364db544313c86d9250166a22d94dce6593e1
baka <dyatlovleonid@gmail.com>
2022-05-11 10:55:19 +0300

    fix

--- api/team.go
+++ api/team.go
@@ -5,24 +5,41 @@ import (
 	"net/http"
 )
 
-func (c Context) createTeam(w http.ResponseWriter, _ *http.Request) {
-	res, err := c.DB.Exec("INSERT INTO teams DEFAULT VALUES")
+func (c Context) createTeam(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		Name   string
+		RoomId int
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(err.Error()))
 		return
 	}
 
-	id, err := res.LastInsertId()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
+	// Handle all internal server errors
+	resp, err := func() ([]byte, error) {
+		res, err := c.DB.Exec("INSERT INTO teams(name, room_id) VALUES(?, ?);", body.Name, body.RoomId)
+		if err != nil {
+			return nil, err
+		}
 
-	resp, err := json.Marshal(struct {
-		ID int `json:"id"`
-	}{ID: int(id)})
+		id, err := res.LastInsertId()
+		if err != nil {
+			return nil, err
+		}
+
+		resp, err := json.Marshal(struct {
+			ID   int64  `json:"id"`
+			Name string `json:"name"`
+		}{ID: id, Name: body.Name})
+		if err != nil {
+			return nil, err
+		}
+
+		return resp, nil
+	}()
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
